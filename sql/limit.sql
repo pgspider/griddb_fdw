e3a037931f89b7744a664d572432b01b6927f9be
Duong Ngoc Lam <lam.duongngoc@toshiba-tsdv.com>
2020-03-05 10:17:18 +0700

    port limit.sql
    Signed-off-by: Duong Ngoc Lam <lam.duongngoc@toshiba-tsdv.com>

--- sql/limit.sql
+++ sql/limit.sql
@@ -24,7 +24,7 @@ CREATE FOREIGN TABLE onek(
   string4     text
 ) SERVER griddb_svr;
 
-CREATE FOREIGN TABLE int8_tbl(id int4 OPTIONS (rowkey 'true'), q1 int8, q2 int8) SERVER griddb_svr;
+CREATE FOREIGN TABLE int8_tbl(id serial OPTIONS (rowkey 'true'), q1 int8, q2 int8) SERVER griddb_svr;
 
 CREATE FOREIGN TABLE tenk1 (
   unique1     int4 OPTIONS (rowkey 'true'),
@@ -81,12 +81,51 @@ select * from int8_tbl offset (case when random() < 0.5 then null::bigint end);
 -- Test assorted cases involving backwards fetch from a LIMIT plan node
 begin;
 
-declare c1 cursor for select * from int8_tbl limit 10;
+declare c1 scroll cursor for select * from int8_tbl limit 10;
 fetch all in c1;
 fetch 1 in c1;
 fetch backward 1 in c1;
+fetch backward all in c1;
+fetch backward 1 in c1;
+fetch all in c1;
+
+declare c2 scroll cursor for select * from int8_tbl limit 3;
+fetch all in c2;
+fetch 1 in c2;
+fetch backward 1 in c2;
+fetch backward all in c2;
+fetch backward 1 in c2;
+fetch all in c2;
+
+declare c3 scroll cursor for select * from int8_tbl offset 3;
+fetch all in c3;
+fetch 1 in c3;
+fetch backward 1 in c3;
+fetch backward all in c3;
+fetch backward 1 in c3;
+fetch all in c3;
+
+declare c4 scroll cursor for select * from int8_tbl offset 10;
+fetch all in c4;
+fetch 1 in c4;
+fetch backward 1 in c4;
+fetch backward all in c4;
+fetch backward 1 in c4;
+fetch all in c4;
 
 rollback;
+
+-- Stress test for variable LIMIT in conjunction with bounded-heap sorting
+BEGIN;
+DELETE FROM INT8_TBL;
+INSERT INTO INT8_TBL(q1) SELECT q1 FROM generate_series(1,10) q1;
+SELECT
+  (SELECT s.q1 
+     FROM (VALUES (1)) AS x,
+          (SELECT q1 FROM INT8_TBL as n 
+             ORDER BY q1 LIMIT 1 OFFSET s.q1-1) AS y) AS z
+  FROM INT8_TBL AS s;
+ROLLBACK;
 
 --
 -- Test behavior of volatile and set-returning functions in conjunction
@@ -126,6 +165,22 @@ select unique1, unique2, generate_series(1,10)
 
 select unique1, unique2, generate_series(1,10)
   from tenk1 order by tenthous limit 7;
+
+-- use of random() is to keep planner from folding the expressions together
+BEGIN;
+DELETE FROM INT8_TBL;
+INSERT INTO INT8_TBL(q1, q2) VALUES (generate_series(0,2), generate_series((random()*.1)::int,2));
+
+explain (verbose, costs off)
+select q1, q2 from INT8_TBL;
+
+select q1, q2 from INT8_TBL;
+
+explain (verbose, costs off)
+select q1, q2 from INT8_TBL order by q2 desc;
+
+select q1, q2 from INT8_TBL order by q2 desc;
+ROLLBACK;
 
 -- test for failure to set all aggregates' aggtranstype
 explain (verbose, costs off)
